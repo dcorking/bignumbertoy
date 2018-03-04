768a2ee7715ed97f3c5e61f978025f40a522c378
David Corking <dmc-source@dcorking.com>
2018-03-04 20:45:31 +0000

    Add a Speak trait

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,18 @@ use bigdecimal::BigDecimal;
 use num_traits::One;
 use num_bigint::BigInt;
 
+// silly trait
+pub trait Speak {
+   fn speak(&self) -> &str;
+}
+
+impl Speak for BigDecimal {
+    fn speak(&self) -> &str {
+        println!("Barking.");
+        "Woof!"
+    }
+}
+
 fn main() {
     // demonstrate BigInt parsing and multiplication
     let billion: BigInt = "1_000_000_000".parse().unwrap();
@@ -15,7 +27,7 @@ fn main() {
 
     // demonstrate BigDecimal constructors, multiplication and division
     let billionth = BigDecimal::one() / BigDecimal::new(billion.clone(), 0);
-    println!("billionth: {}", &billionth);
+    println!("\nbillionth: {}", &billionth);
     println!("billionth of a billionth: {}", &billionth * & billionth);
     println!("billionth of a billionth of a billionth: {} or {}",
              &billionth * &billionth * & billionth,
@@ -24,11 +36,14 @@ fn main() {
     // demonstrate the difference between scaling and division
     let thirty_digits: BigDecimal = "123456789012345678901234567890".parse().unwrap();
     let downscaled_thirty = thirty_digits.clone().with_scale(-18);
-    println!("If you downscale a BigDecimal like this: {},\nyou will lose precision and end up with this: {}", &thirty_digits, &downscaled_thirty);
+    println!("\nIf you downscale a BigDecimal like this: {},\nyou will lose precision and end up with this: {}", &thirty_digits, &downscaled_thirty);
     println!("which are represented internally as\n {:#?}, and\n {:#?}", &thirty_digits, &downscaled_thirty);
     let billion_billion = BigDecimal::new(&billion * &billion, 0);
     let divided_thirty = &thirty_digits / &billion_billion;
-    println!("So instead of downscaling, divide it by {}\n to get {}\n, represented internally as\n {:#?}, and\n {:#?} ",
+    println!("So instead of downscaling, divide it by {}\n to get {}\n, represented internally as\n {:#?}, and\n {:#?} \n",
              &billion_billion, &divided_thirty,
              &billion_billion, &divided_thirty);
+
+    let sound = billion_billion.speak();
+    println!("The sound it made: {}", sound);
 }
